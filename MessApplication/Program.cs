4f7acbc19ad24708a20764d245c82aeb4760646b
= <->
2026-04-14 23:46:25 +0530

    change in cors

--- MessApplication/Program.cs
+++ MessApplication/Program.cs
@@ -79,7 +79,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("AllowFrontend");
+app.UseCors("AllowAll");
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
